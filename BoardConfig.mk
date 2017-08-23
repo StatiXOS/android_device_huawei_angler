ad4e12af5f10b51be7b6d3ddc8bef4f1a862cd1d
ezio84 <brabus84@gmail.com>
2017-08-23 16:21:51 +0200

    Custom kernel config [2/2]

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -33,7 +33,7 @@ TARGET_2ND_CPU_VARIANT := cortex-a53.a57
 KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.9/bin
 KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-android-
 TARGET_KERNEL_SOURCE := kernel/huawei/angler
-TARGET_KERNEL_CONFIG := angler_defconfig
+TARGET_KERNEL_CONFIG := abc_defconfig
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 
 BOARD_KERNEL_BASE        := 0x00000000
