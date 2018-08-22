7d5ae297e68781455ec22f75f41101f8c59e73ab
Jertlok <jertlokk@gmail.com>
2018-08-22 05:41:56 +0000

    Media profiles need to go inside /system.
    This commit should have be done by @anayw2001 and fixes Camera2
    
    Signed-off-by: Jertlok <jertlokk@gmail.com>

--- device.mk
+++ device.mk
@@ -56,12 +56,12 @@ PRODUCT_COPY_FILES += \
 
 # Media
 PRODUCT_COPY_FILES += \
-    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml \
-    device/huawei/angler/configs/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
-    device/huawei/angler/configs/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
-    device/huawei/angler/configs/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles.xml
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$system/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$system/etc/media_codecs_google_video.xml \
+    device/huawei/angler/configs/media_codecs.xml:$system/etc/media_codecs.xml \
+    device/huawei/angler/configs/media_codecs_performance.xml:$system/etc/media_codecs_performance.xml \
+    device/huawei/angler/configs/media_profiles.xml:$system/etc/media_profiles.xml
 
 # Audio
 PRODUCT_COPY_FILES += \
