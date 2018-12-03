cce97d50e37e5c4aa87dc45a6dc18fbcddc16075
Anay Wadhera <anay1018@gmail.com>
2018-12-03 16:30:44 -0600

    never thought i'd do this but angler: update fingerprint to December
    Change-Id: I283491e4e46ff68e8603b069f269101f4d5dfcbc

--- statix_angler.mk
+++ statix_angler.mk
@@ -36,6 +36,6 @@ PRODUCT_MANUFACTURER := Huawei
 # Device Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=angler \
-    PRIVATE_BUILD_DESC="angler-user 8.1.0 OPM7.181105.004 5038062 release-keys"
+    PRIVATE_BUILD_DESC="angler-user 8.1.0 OPM7.181205.001 5080180 release-keys"
 
-BUILD_FINGERPRINT :=  google/angler/angler:8.1.0/OPM7.181105.004/5038062:user/release-keys
+BUILD_FINGERPRINT := google/angler/angler:8.1.0/OPM7.181205.001/5080180:user/release-keys
