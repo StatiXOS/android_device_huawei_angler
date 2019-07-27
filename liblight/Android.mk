bf0321ba5dc0f1a64f1792beca6f32a2f80b387c
Anay Wadhera <anay1018@gmail.com>
2019-07-27 15:38:20 -0700

    angler: move power and lights to vendor
    Change-Id: I7490ab594beb9479af9efbf12ae9660cc16b9bab

--- liblight/Android.mk
+++ liblight/Android.mk
@@ -25,5 +25,6 @@ LOCAL_SHARED_LIBRARIES := liblog
 LOCAL_CFLAGS := $(common_flags) -DLOG_TAG=\"anglerlights\"
 LOCAL_MODULE := lights.angler
 LOCAL_MODULE_TAGS := optional
+LOCAL_VENDOR_MODULE := true
 
 include $(BUILD_SHARED_LIBRARY)
