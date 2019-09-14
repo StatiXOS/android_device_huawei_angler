430be943d0e8cac6f8337ad046aa5829ae4fc79d
J0SH1X <aljoshua.hell@gmail.com>
2019-09-14 18:15:11 +0200

    Angler: Fix build fingerprint missmatch
    ro.build.fingerprint got generated by build system and so missmatched to vendor and boot image, thus showing a missmatch error every boot.
    Override this property to match them.

--- statix_angler.mk
+++ statix_angler.mk
@@ -40,3 +40,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="angler-user 8.1.0 OPM7.181205.001 5080180 release-keys"
 
 BUILD_FINGERPRINT := google/angler/angler:8.1.0/OPM7.181205.001/5080180:user/release-keys
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.fingerprint=google/angler/angler:8.1.0/OPM7.181205.001/5080180:user/release-keys
