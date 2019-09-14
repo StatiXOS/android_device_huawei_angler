b26d91016c303dc4dd45b83909b36eacd3c55d6d
PixelBoot <bootingpixels@gmail.com>
2019-09-14 09:30:15 +0200

    angler: Update build fingerprint to Dec 2018 OPM7.181205.001

--- statix_angler.mk
+++ statix_angler.mk
@@ -37,6 +37,6 @@ PRODUCT_MANUFACTURER := Huawei
 # Device Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=angler \
-    PRIVATE_BUILD_DESC="angler-user 8.1.0 OPM3.171019.014 4503998 release-keys"
+    PRIVATE_BUILD_DESC="angler-user 8.1.0 OPM7.181205.001 5080180 release-keys"
 
-BUILD_FINGERPRINT := google/angler/angler:8.1.0/OPM3.171019.014/4503998:user/release-keys
+BUILD_FINGERPRINT := google/angler/angler:8.1.0/OPM7.181205.001/5080180:user/release-keys
